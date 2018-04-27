cb113b9c8a5c961be58d300ef19b2a20ebdb9820
Rock Hu <rockia@mac.com>
2018-04-26 21:51:25 -0700

    Add counter state and button to modify state

--- App.js
+++ App.js
@@ -1,38 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Counter Value: {this.state.counter}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button 
+          title="Press me"
+          style={styles.button}
+          onPress={()=>{this.setState({counter: this.state.counter + 1})}}
+        />
       </View>
     );
   }
@@ -50,9 +43,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  button: {
+    textAlign: 'center'
   },
 });
